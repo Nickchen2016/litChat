4402f62c4b95f3d1ffaebcfa1b16ffad8a28dac5
Nick Chen <chenyahua2012@hotmail.com>
2020-06-03 15:42:04 -0400

    initil setup typescript-react-webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader'
+                }
+              }
+        ]
+    },
+    devtool: 'source-maps',
+    resolve: {
+        extensions: ['.ts','.tsx', '.js', '.json'] // Add '.ts|.tsx' as a resolvable extension, and let the upper ts-loader to handle to compilation. 
+    },
+    mode: 'development'
+    // plugins: [
+
+    // ]
+}
