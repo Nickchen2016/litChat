a0e6a1066e846cdbc5a32bbe802fa920bfcd26e7
Nick Chen <chenyahua2012@hotmail.com>
2020-06-03 18:41:21 -0400

    Successfully pass props to comment.js from App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Typing } from './Typing';
+import { Comment } from './Comment';
+import './app.scss';
 
+const initialComment: Array<CommentStructure> = [
+    {text: 'Hey, Nick, hows goin?', who: '1'},
+    {text: 'Yo, I am doing well tho', who: '2'}
+]
 
 export default function App() {
-    return <div>hello world!</div>
+
+    const [comments, setcomment] = useState(initialComment);
+
+    const addComment:AddCommentFunc = data => {
+        setcomment([...comments, data]);
+    }
+
+    return (
+        <div id='chat_window'>
+            <Comment comments={comments}/>
+            <Typing addComment={addComment}/>
+        </div>
+    )
 }
