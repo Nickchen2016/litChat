db168d8a948ebf6c2785fe64423592fdcdc5ee39
Nick Chen <chenyahua2012@hotmail.com>
2020-06-04 17:39:13 -0400

    db connected

--- server/index.js
+++ server/index.js
@@ -5,9 +5,30 @@ const bodyParser = require('body-parser');
 
 const PORT = 5000;
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const uri = `mongodb+srv://iconic:${process.env.PASS}@litchat-rtwxj.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+
+async function mgdb() {
+    try {
+      await mongoose.connect(uri, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      });
+      console.log('connected!');
+    } catch (err) {
+      console.log(err);
+    }
+}
+
+mgdb();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 
 app.get('/', (req,res)=>{
